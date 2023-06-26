c03b01cbe11eafd73691a57f447745af567d3252
Furkan Leylek <furkanyasinn.1@outlook.com>
2023-06-26 18:27:37 +0300

    bg

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -7,7 +7,7 @@ const MenuPage = () => {
         <section
             className="w-full h-full bg-primaryBg flex flex-col items-center justify-center "
         >
-            <div className='fixed top-0 z-20 bg-primary w-full'>
+            <div className='fixed top-0 z-20 bg-secondaryBg w-full'>
                 <Header />
             </div>
             <div className='w-full flex flex-col gap-16 xl:gap-32 mt-20 mdlg:mt-[280px] xl:mt-[320px] container mx-auto pb-12 xl:pb-32 md:px-16 xl:px-32 relative '
